bd458b1997181af4568fc1187299e1a706ea6368
aslobodnik <aslobodnik@gmail.com>
2024-01-31 11:47:17 -0500

    add arb ecosystem data

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -2,77 +2,47 @@ import { ContractInfo } from "../types/types";
 
 export const multiSigs: ContractInfo[] = [
   {
-    address: "0xcD42b4c4D102cc22864e3A1341Bb0529c17fD87d",
-    label: "Public Goods",
+    address: "0xC158b555F0B1ddd7F4D4f97Fd2a9acd144f8e0D4",
+    label: "Plurality Labs",
   },
   {
-    address: "0x2686A8919Df194aA7673244549E68D42C1685d03",
-    label: "Ecosystem",
+    address: "0x5a5c8Da41220EeF8E7f423f6e9028da5b9E70f8B",
+    label: "STIP",
   },
   {
-    address: "0x6a016548310076285668e2378df70bd545396b5a",
-    label: "Ecosystem",
+    address: "0x524E480DC669592757B3f22140Cc0950a9C2f489",
+    label: "Procurement Committee",
   },
   {
-    address: "0x536013c57DAF01D78e8a70cAd1B1abAda9411819",
-    label: "Ecosystem",
+    address: "0xff1f38f8cDae7C1B1B289FCF6f5C1AA234DF1443",
+    label: "Delegates Incentives System",
   },
   {
-    address: "0x9B9c249Be04dd433c7e8FbBF5E61E6741b89966D",
-    label: "Ecosystem",
+    address: "0xd19Ed0E8E723fDb85a6a1480e5345FbCcE0BFF85",
+    label: "Research and Development Collective",
   },
   {
-    address: "0x0d06a817584ac378849f03df6f11a9ad67dd786d",
-    label: "Ecosystem",
+    address: "0x8ac412F1eB56B01ba910C71f4Fad1c7f70Efb4E5",
+    label: "The Rariable Protocol",
   },
   {
-    address: "0x13aEe52C1C688d3554a15556c5353cb0c3696ea2",
-    label: "Ecosystem",
+    address: "0x120C1920656188d4F00F6De37D1Da7fdCcAC4A9A",
+    label: "Questbook: New Protocols and Ideas",
   },
   {
-    address: "0x91c32893216dE3eA0a55ABb9851f581d4503d39b",
-    label: "Metagov",
+    address: "0x3dfe378547104ddf6e80ff26d1681a6d7a9f1af0",
+    label: "Questbook: Dev Tooling on One and NOVA",
   },
   {
-    address: "0xBa0c461b22d918FB1F52fEF556310230d177D1F2",
-    label: "Ecosystem",
+    address: "0x1191eFE82884eA1089AC51E894d1559df2E350C0",
+    label: "Questbook: Education, Community Growth and Events",
   },
   {
-    address: "0x4f4cAdb8AF8F1d463240c2b93952D8a16688a818",
-    label: "Metagov",
+    address: "0xebe67F2ceD2780321903CA7C6602C914262eb2DC",
+    label: "Questbook: Arbitrum Gaming",
   },
   {
-    address: "0x8f730f4aC5fd234df9993E0E317f07e44fb869C1",
-    label: "Metagov",
-  },
-  {
-    address: "0x9718ba71dC1284842fcE66dC3e34DFFC6C630074",
-    label: "Ecosystem",
-  },
-
-  {
-    address: "0xB162Bf7A7fD64eF32b787719335d06B2780e31D1",
-    label: "Metagov",
-  },
-];
-
-export const opsContracts: ContractInfo[] = [
-  {
-    address: "0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5",
-    label: "ENS Controller",
-    description: "Collects ETH from ENS registrations",
-  },
-  {
-    address: "0xFe89cc7aBB2C4183683ab71653C4cdc9B02D44b7",
-    label: "ENS Wallet",
-    description: "Stores DAO funds",
-  },
-];
-
-export const endowment: ContractInfo[] = [
-  {
-    address: "0x4F2083f5fBede34C2714aFfb3105539775f7FE64",
-    label: "Endowment",
-    description: "Endowment invests DAO funds",
+    address: "0xB7c1551BBEB47Eb86266153df5F9D7dA47F08308",
+    label: "Questbook: Program Manager",
   },
 ];
